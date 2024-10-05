66a32d1b022ac8268802d2b0b0b51d9909a02dff
laYSa-Bispo <laysa.silva@sptech.school>
2024-10-04 23:13:44 -0300

    Script BD

--- database/script.sql
+++ database/script.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE Helpers;
 USE Helpers;
 
-CREATE TABLE empresa(
+CREATE TABLE Empresa(
 	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
     razaoSocial VARCHAR(45) NOT NULL,
     CNPJ CHAR(14) UNIQUE NOT NULL,
@@ -9,15 +9,15 @@ CREATE TABLE empresa(
     responsavel VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE login(
-	idLogin INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Usuario(
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     userName VARCHAR(45),
 	senha VARCHAR(45) NOT NULL,
 	fKEmpresa INT,
     CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
-CREATE TABLE endereco(
+CREATE TABLE Endereco(
 	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
 	CEP CHAR(14),
 	numero VARCHAR(45),
@@ -25,9 +25,38 @@ CREATE TABLE endereco(
 	CONSTRAINT fkEmpresaEnd FOREIGN KEY (fkEmpresaEnd) REFERENCES empresa(idEmpresa)
 );
 
-CREATE TABLE acesso(
+CREATE TABLE Acesso(
 	idAcesso INT PRIMARY KEY AUTO_INCREMENT,
 	dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-	fkLogin INT,
-	CONSTRAINT fkLogin FOREIGN KEY (fkLogin) REFERENCES login(idLogin)
+	fkUsuario INT,
+	CONSTRAINT fkAcessoUsuario FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario)
+);
+
+CREATE TABLE Sensor (
+idSensor INT PRIMARY KEY AUTO_INCREMENT, 
+Nome VARCHAR(45),
+numeroSerie VARCHAR(45),
+fkEmpresa INT,
+	CONSTRAINT fkSensorEmpresa FOREIGN KEY (fkEmpresa)
+		REFERENCES Empresa(idEmpresa),
+fkMaquina INT,
+	CONSTRAINT fkSensorMaquina FOREIGN KEY (fkMaquina)
+		REFERENCES Maquina(idMaquina)
+);
+
+CREATE TABLE Registro (
+idRegistro INT PRIMARY KEY AUTO_INCREMENT,
+dataHora DATETIME,
+Temperatura DECIMAL(5,2),
+fkSensor INT,
+	CONSTRAINT fkRegistroSensor FOREIGN KEY (fkSensor)
+		REFERENCES Sensor(idSensor)
+);
+
+CREATE TABLE Maquina (
+idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+Nome VARCHAR(45),
+Tipo VARCHAR(45),
+tempMax DECIMAL(5,2),
+Setor VARCHAR(45)
 );
