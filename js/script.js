2f156bb46cded9ad516efbf35d4a98518c750ac4
souMarioJunior <mario.aaraujojr@sptech.school>
2024-10-08 23:11:40 -0300

    update tela de cadastro dinâmica

--- js/script.js
+++ js/script.js
@@ -37,3 +37,43 @@ function valSenha(){
 
 
 }
+
+function step_01() {
+    var box_form_1 = document.getElementById('form_1');
+    var box_form_2 = document.getElementById('form_2');
+    var box_form_3 = document.getElementById('form_3');
+
+    box_form_1.style.display = 'none';
+    box_form_2.style.display = 'block';
+    box_form_3.style.display = 'none';
+}
+
+function step_02() {
+    var box_form_1 = document.getElementById('form_1');
+    var box_form_2 = document.getElementById('form_2');
+    var box_form_3 = document.getElementById('form_3');
+
+    box_form_1.style.display = 'none';
+    box_form_2.style.display = 'none';
+    box_form_3.style.display = 'block';
+}
+
+function retornar_tela_01() {
+    var box_form_1 = document.getElementById('form_1');
+    var box_form_2 = document.getElementById('form_2');
+    var box_form_3 = document.getElementById('form_3');
+
+    box_form_1.style.display = 'block';
+    box_form_2.style.display = 'none';
+    box_form_3.style.display = 'none';
+}
+
+function retornar_tela_02() {
+    var box_form_1 = document.getElementById('form_1');
+    var box_form_2 = document.getElementById('form_2');
+    var box_form_3 = document.getElementById('form_3');
+
+    box_form_1.style.display = 'none';
+    box_form_2.style.display = 'block';
+    box_form_3.style.display = 'none';
+}
