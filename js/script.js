c3547567de7aebe800623373a77ecb64fd4fbc36
souMarioJunior <mario.aaraujojr@sptech.school>
2024-10-10 13:59:46 -0300

    Alert de validação de senha

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+var guardarSenha = "";
+var validouSenha = false;
+// var validouSenha =
 function valSenha() {
+  validouSenha = false;
   var input_senha = document.getElementById("ipt_senha");
+  var input_confirmar_senha = document.getElementById("ipt_confirmar_senha");
   var senha = input_senha.value;
+  var confirmar_senha = input_confirmar_senha.value;
   var tamanho = senha.length;
   var esp = false;
   var num = false;
   var min = false;
   var max = false;
   var validos = "!@#$%&?";
-  validacao.innerHTML = "";
+  // validacao.innerHTML = "";
+  var mensagem = "";
+  var mensagem_senhas_diferentes = "";
 
   for (var i = 0; i < tamanho; i++) {
     for (var j = 0; j < validos.length; j++) {
@@ -27,27 +35,60 @@ function valSenha() {
   }
 
   if (tamanho < 8 || tamanho > 30)
-    validacao.innerHTML += `A senha deve conter entre 8 e 30 caracteres <br>`;
+    mensagem += `A senha deve conter entre 8 e 30 caracteres <br>`;
 
-  if (!num) validacao.innerHTML += `A senha deve conter 1 número <br>`;
+  if (!num) mensagem += `A senha deve conter 1 número <br>`;
 
   if (!esp)
-    validacao.innerHTML += `A senha deve conter 1 caracter especial: '!@#$%&?' <br>`;
+    mensagem += `A senha deve conter 1 caracter especial: '!@#$%&?' <br>`;
 
-  if (!min) validacao.innerHTML += `A senha deve conter 1 letra minúscula`;
+  if (!min) mensagem += `A senha deve conter 1 letra minúscula <br>`;
 
-  if (!max) validacao.innerHTML += `A senha deve conter 1 letra maiúscula`;
+  if (!max) mensagem += `A senha deve conter 1 letra maiúscula <br>`;
 
   //  validacao.innerHTML += `A senha deve conter 1 letra minúscula`
 
   // Senha Vazia
   // if (senha == '') validacao.innerHTML = '';
-  if (senha == "") validacao.innerHTML = `Preencha o campo para continuar`;
+  if (senha == "") mensagem = `Preencha o campo para continuar <br>`;
   input_senha.style.outline = "none";
   input_senha.style.border = "solid red 2px";
   if (num && esp && min && max) {
     input_senha.style.outline = "solid #22c55e 2px";
     input_senha.style.border = "#22c55e";
+    guardarSenha = ``;
+    guardarSenha = senha;
+    validouSenha = true;
+  }
+
+  validacao.innerHTML = mensagem;
+}
+
+function continuar() {
+  var alert_modal = document.getElementById("modal");
+
+  if (validouSenha) {
+    alert_modal.style.display = "flex";
+    alert_modal.innerHTML = `
+            <div class="box_modal">
+          <div class="icon">
+            <img
+              height="140px"
+              src="../assets/verified_high_quality.gif"
+              alt="Imagem de verificação aprovada"
+            />
+          </div>
+          <div class="titulo">
+            <h2>Cadastro efetuado com sucesso!</h2>
+          </div>
+          <div class="info">
+            <span>Clique no botão de continuar para ir para a tela de login</span>
+          </div>
+          <div class="btn">
+            <a href="paglogin.html">Continuar</a>
+          </div>
+        </div>
+    `;
   }
 }
 
