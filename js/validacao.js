8073384c959fcb2b3bf11ee53159d696c91b627b
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-10-10 22:12:44 -0300

    val

---
+++ js/validacao.js
@@ -0,0 +1,86 @@
+function valSenha() {
+  var senha = document.getElementById("ipt_senha").value;
+  var tamanho = senha.length;
+  var esp = false;
+  var num = false;
+  var min = false;
+  var max = false;
+  var tam = false;
+  var validos = "!@#$%&?";
+  var mensagem = "";
+
+  for (var i = 0; i < tamanho; i++) {
+    for (var j = 0; j < validos.length; j++) {
+      if (senha[i] == validos[j]) {
+        esp = true;
+      }
+      if (!isNaN(senha[i])) {
+        num = true;
+      }
+    }
+    if (senha[i].toUpperCase() != senha[i]) {
+      min = true;
+    }
+    if (senha[i].toLowerCase() != senha[i]) {
+      max = true;
+    }
+  }
+
+  if (tamanho < 8 || tamanho > 30) {
+    mensagem += `A senha deve conter entre 8 e 30 caracteres <br>`;
+  } else {
+    tam = true;
+  }
+
+  if (!num) mensagem += `A senha deve conter 1 número <br>`;
+
+  if (!esp)
+    mensagem += `A senha deve conter 1 caracter especial: '!@#$%&?' <br>`;
+
+  if (!min) mensagem += `A senha deve conter 1 letra minúscula <br>`;
+
+  if (!max) mensagem += `A senha deve conter 1 letra maiúscula <br>`;
+
+  validacao.innerHTML = mensagem;
+
+  if (senha == "") mensagem = `Preencha o campo para continuar <br>`;
+  ipt_senha.style.outline = "none";
+  ipt_senha.style.border = "solid red 2px";
+  if (num && esp && min && max && tam) {
+    ipt_senha.style.outline = "solid #22c55e 2px";
+    ipt_senha.style.border = "#22c55e";
+  }
+
+  var confirmSenha = ipt_confirmar_senha.value;
+  var senhasIguais = confirmSenha == senha;
+  if (!confirmSenha == "") {
+    if (senhasIguais) {
+      ipt_confirmar_senha.style.outline = "solid #22c55e 2px";
+      ipt_confirmar_senha.style.border = "#22c55e";
+    } else {
+      validacao.innerHTML += `As senhas não batem`;
+      ipt_confirmar_senha.style.outline = "none";
+      ipt_confirmar_senha.style.border = "solid red 2px";
+    }
+  } else {
+    ipt_confirmar_senha.style.outline = "none";
+    ipt_confirmar_senha.style.border = "solid red 2px";
+  }
+}
+
+function valRazaoSocial() {
+  var razao = document.getElementById("ipt_razao").value;
+
+  var termina =
+    razao.endsWith("ME") ||
+    razao.endsWith("S.A") ||
+    razao.endsWith("LTDA") ||
+    razao.endsWith("EPP")
+      ? true
+      : false;
+
+  if (!termina) {
+    console.log('if')
+    validacao.innerHTML = 'teste';
+  }
+}
