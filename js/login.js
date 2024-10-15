474b53396cf96bc646acfe947acf3a2373165d09
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-10-15 00:27:16 -0300

    Login de teste funcional

--- js/login.js
+++ js/login.js
@@ -1,3 +1,14 @@
 function login(){
+    var senhaTeste = 'T3st#123';
+    var usuarioTeste = 'Urubu100';
     
+    var usuario = document.getElementById('ipt_usuario').value;
+    var senha = document.getElementById('ipt_senhaLogin').value;
+
+    if(usuario == usuarioTeste && senha == senhaTeste){
+        alert('Login realizado com sucesso!');
+        window.location.href = 'dashboard.html';
+    } else {
+        alert('Usuário ou senha inválidos!');
+    }
 }
