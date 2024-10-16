4b48f9bb1130f992ae3d41e2cf211dceaeea66d5
laYSa-Bispo <laysa.silva@sptech.school>
2024-10-16 16:17:40 -0300

    Configuração do Gráfico 3

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,6 +8,7 @@ const gradient = ctx.getContext("2d").createLinearGradient(0, 0, 0, 400);
 gradient.addColorStop(0.1, "rgba(255,240,0,1)"); // Cor amarela
 gradient.addColorStop(0.9, "rgba(255,170,0,1)"); // Cor laranja
 gradient.addColorStop(0.5, "rgba(219,89,6,1)"); // Cor vermelha
+Chart.defaults.color = '#FFF';
 
 new Chart(ctx, {
   type: "bar",
@@ -90,40 +91,29 @@ new Chart(ctx2, {
   },
 });
 
+
 new Chart(ctx3, {
-  type: "bar",
+  type: "line",
   data: {
-    labels: [
-      "Sensor 1",
-      "Sensor 2",
-      "Sensor 3",
-      "Sensor 4",
-      "Sensor 5",
-      "Sensor 6",
-      "Sensor 7",
-      "Sensor 8",
-      "Sensor 9",
-      "Sensor 10",
-      "Sensor 11",
-      "Sensor 12",
-    ],
+    labels: ["8:00", "9:00", "10:00", "11:00", "12:00", '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'],
     datasets: [
       {
-        label: "Temperatura Média por Sensor",
-        data: [90, 95, 110, 100, 115, 98, 90, 95, 110, 100, 115, 98],
-        borderWidth: 1,
-        backgroundColor: gradient, // Use o gradiente como cor de fundo
+        label: "Temperatura indiviludal do Sensor 1",
+        data: [90, 95, 110, 100, 115, 98, 100, 95, 110, 100, 115],
+        borderWidth: 2,
+        backgroundColor: 'rgba(219,89,6,1)', // Use o gradiente como cor de fundo
+        borderColor: 'rgba(219,89,6,1)',
+        color: '#fff',
       },
     ],
   },
   options: {
     scales: {
-      y: {
+      x: {
         beginAtZero: true,
-        max: 150, // Define a escala máxima do eixo Y
-        ticks: {
-          stepSize: 10, // Define o intervalo do eixo Y para 10 em 10
-        },
+      },
+      y: {
+        max: 130,
       },
     },
   },
