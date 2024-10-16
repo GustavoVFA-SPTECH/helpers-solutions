e126452cc4d0f1ae55bc3d72ab25fe213962fc41
laYSa-Bispo <laysa.silva@sptech.school>
2024-10-16 16:01:22 -0300

    Inserção dos novos gráificos

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,9 @@
 const ctx = document.getElementById("myChart");
 const ctx2 = document.getElementById("myChart_2");
+const ctx3 = document.getElementById("myChart_3");
+const ctx4 = document.getElementById("myChart_4");
 
-// Criação do gradiente com cores invertidas
+// Criação do gradiente
 const gradient = ctx.getContext("2d").createLinearGradient(0, 0, 0, 400);
 gradient.addColorStop(0.1, "rgba(255,240,0,1)"); // Cor amarela
 gradient.addColorStop(0.9, "rgba(255,170,0,1)"); // Cor laranja
@@ -46,6 +48,8 @@ new Chart(ctx, {
   },
 });
 
+
+
 new Chart(ctx2, {
   type: "bar",
   data: {
@@ -76,6 +80,84 @@ new Chart(ctx2, {
     indexAxis: "y",
     scales: {
       x: {
+        beginAtZero: true,
+        max: 150, // Define a escala máxima do eixo Y
+        ticks: {
+          stepSize: 10, // Define o intervalo do eixo Y para 10 em 10
+        },
+      },
+    },
+  },
+});
+
+new Chart(ctx3, {
+  type: "bar",
+  data: {
+    labels: [
+      "Sensor 1",
+      "Sensor 2",
+      "Sensor 3",
+      "Sensor 4",
+      "Sensor 5",
+      "Sensor 6",
+      "Sensor 7",
+      "Sensor 8",
+      "Sensor 9",
+      "Sensor 10",
+      "Sensor 11",
+      "Sensor 12",
+    ],
+    datasets: [
+      {
+        label: "Temperatura Média por Sensor",
+        data: [90, 95, 110, 100, 115, 98, 90, 95, 110, 100, 115, 98],
+        borderWidth: 1,
+        backgroundColor: gradient, // Use o gradiente como cor de fundo
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        max: 150, // Define a escala máxima do eixo Y
+        ticks: {
+          stepSize: 10, // Define o intervalo do eixo Y para 10 em 10
+        },
+      },
+    },
+  },
+});
+
+new Chart(ctx4, {
+  type: "bar",
+  data: {
+    labels: [
+      "Sensor 1",
+      "Sensor 2",
+      "Sensor 3",
+      "Sensor 4",
+      "Sensor 5",
+      "Sensor 6",
+      "Sensor 7",
+      "Sensor 8",
+      "Sensor 9",
+      "Sensor 10",
+      "Sensor 11",
+      "Sensor 12",
+    ],
+    datasets: [
+      {
+        label: "Temperatura Média por Sensor",
+        data: [90, 95, 110, 100, 115, 98, 90, 95, 110, 100, 115, 98],
+        borderWidth: 1,
+        backgroundColor: gradient, // Use o gradiente como cor de fundo
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
         beginAtZero: true,
         max: 150, // Define a escala máxima do eixo Y
         ticks: {
