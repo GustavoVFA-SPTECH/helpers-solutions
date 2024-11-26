abc5961f203f65fda08f0220fdffed3a3ac5d6e1
GuilhermeEduardoGS <guilherme.gsouza@sptech.school>
2024-11-26 12:56:23 -0300

    Select com View no Script

--- src/database/script.sql
+++ src/database/script.sql
@@ -12,14 +12,10 @@ CREATE TABLE Empresa (
     responsavel VARCHAR(45) NOT NULL
 );
 
-INSERT INTO Empresa (razaoSocial, CNPJ, email, responsavel
+INSERT INTO Empresa (razaoSocial, CNPJ, telefone, email, userName, senha, responsavel
 ) VALUES
-('Empresa A', '12345678000195', 'contato@empresaa.com', 'João Silva'),
-('Empresa B', '98765432000198', 'contato@empresab.com', 'Maria Oliveira');
-
-INSERT INTO Usuario (userName, senha, fKEmpresa) VALUES
-('usuario1', 'Senha@123', 1),
-('usuario2', 'Senha456!', 2);
+('Empresa A', '12345678000195', '1234567890', 'contato@empresaa.com', 'Usuario1', 'Urubu@100', 'João Silva'),
+('Empresa B', '98765432000198', '0987654321', 'contato@empresab.com', 'Usuario2', 'Urubu@200', 'Maria Oliveira');
 
 CREATE TABLE Endereco (
     idEndereco INT PRIMARY KEY AUTO_INCREMENT,
@@ -44,14 +40,9 @@ INSERT INTO Endereco (CEP, Logradouro, Numero, Complemento, Bairro, Cidade, Esta
 CREATE TABLE Acesso (
     idAcesso INT PRIMARY KEY AUTO_INCREMENT,
     dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkUsuario INT,
-    CONSTRAINT fkAcessoUsuario FOREIGN KEY (fkUsuario) REFERENCES Usuario (idUsuario)
+    fkEmpresa INT,
+    CONSTRAINT fkAcessoEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
-
-INSERT INTO Acesso (fkUsuario) VALUES
-(1),
-(2);
-
 
 CREATE TABLE Sensor (
     idSensor INT PRIMARY KEY AUTO_INCREMENT,
@@ -60,21 +51,27 @@ CREATE TABLE Sensor (
 );
 
 INSERT INTO Sensor (Nome, numeroSerie) VALUES
-('Sensor A', 'SN123456'),
-('Sensor B', 'SN654321');
+('Sensor 1', 'SN123456'),
+('Sensor 2', 'SN654321'),
+('Sensor 3', 'SN456789'),
+('Sensor 4', 'SN987654'),
+('Sensor 5', 'SN321654');
 
 
 CREATE TABLE Registro (
     idRegistro INT PRIMARY KEY AUTO_INCREMENT,
     dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    Temperatura DECIMAL(5, 2),
+    Temperatura FLOAT,
     fkSensor INT,
     CONSTRAINT fkRegistroSensor FOREIGN KEY (fkSensor) REFERENCES Sensor (idSensor)
 );
 
 INSERT INTO Registro (Temperatura, fkSensor) VALUES
-(141.50, 1),
-(115.00, 2);
+(114.00, 1),
+(20.00, 2),
+(111.50, 3),
+(110.00, 4),
+(200.00, 5);
 
 CREATE TABLE Maquina (
     idMaquina INT PRIMARY KEY AUTO_INCREMENT,
@@ -89,8 +86,11 @@ CREATE TABLE Maquina (
 );
 
 INSERT INTO Maquina (Nome, Tipo, tempMax, Setor, fkEmpresa, fkSensor) VALUES
-('Máquina A', 'Tipo A', 75.00, 'Setor A', 1, 1),
-('Máquina B', 'Tipo B', 80.00, 'Setor B', 2, 2);
+('Máquina A', 'Tipo A', 150.00, 'Setor A', 1, 1),
+('Máquina B', 'Tipo B', 150.00, 'Setor B', 2, 2),
+('Máquina B', 'Tipo B', 150.00, 'Setor B', 2, 3),
+('Máquina A', 'Tipo A', 150.00, 'Setor A', 1, 4),
+('Máquina B', 'Tipo B', 150.00, 'Setor B', 2, 5);
 
 
 SELECT * FROM Registro;
@@ -120,3 +120,22 @@ JOIN Sensor as s
 ON r.fkSensor = s.idSensor
 JOIN Maquina as m
 ON m.fkSensor = s.idSensor;
+
+-- VIEW
+CREATE VIEW RegistroMaquina
+as
+SELECT r.temperatura as Temperatura, r.dataHora as Horário, m.nome as Máquina,
+CASE
+WHEN Temperatura < 100
+THEN 'Resfriamento'
+WHEN Temperatura < 140
+THEN 'OK'
+ELSE 'Superaquecimento'
+END AS Stats
+FROM Registro as r
+JOIN Sensor as s
+ON r.fkSensor = s.idSensor
+JOIN Maquina as m
+ON m.fkSensor = s.idSensor;
+
+select Horário, Máquina, Temperatura, stats from RegistroMaquina;
