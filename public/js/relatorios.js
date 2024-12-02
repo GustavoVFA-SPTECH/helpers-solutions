195bd6906032845c53b159c7a89ed75f85ebf926
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2024-12-02 12:19:55 -0300

    relatorios 1

--- public/js/relatorios.js
+++ public/js/relatorios.js
@@ -1,27 +1,47 @@
-function carregarRelatorios(){   
-    fetch("/relatorios/relatorios").then(function (resposta){
-        if(resposta.ok){
-            if(resposta.status == 204){
+function carregarRelatorios() {
+    fetch("/relatorios/relatorios").then(function (resposta) {
+        if (resposta.ok) {
+            if (resposta.status == 204) {
                 throw "Nenhum resultado encontrado!";
             }
 
-            resposta.json().then(function(resposta){
-                console.log("Resposta recebida: ", JSON.stringify(resposta));
-                
-                
-                for(var i = 0; i < resposta.length; i++){
-                    var formmatDate = new Date(resposta[i].horario).toLocaleDateString("PT-BR", {hour: "2-digit", minute: "2-digit"})
-                    dadosHorarios.innerHTML += `${formmatDate}<br>`
-                    dadosMaq.innerHTML += `${resposta[i].maquina}<br>`
-                    dadosTemp.innerHTML += `${resposta[i].temp}<br>`
-                    dadosStatus.innerHTML += `${resposta[i].Stats}<br>`
+            resposta.json().then(function (dados) {
+                console.log("Resposta recebida: ", JSON.stringify(dados));
+                const tabela = document.querySelector('.relatorios');
+                tabela.querySelectorAll('tr:not(:first-child)').forEach(function (linha) {
+                    linha.remove();
+                });
+                for (var i = 0; i < dados.length; i++) {
+                    var formatoData = new Date(dados[i].horario).toLocaleDateString("PT-BR", { hour: "2-digit", minute: "2-digit" });
+
+                    var novaLinha = document.createElement('tr');
+
+                    var celulaHorario = document.createElement('td');
+                    celulaHorario.textContent = formatoData;
+
+                    var celulaMaquina = document.createElement('td');
+                    celulaMaquina.textContent = dados[i].maquina;
+
+                    var celulaTemperatura = document.createElement('td');
+                    celulaTemperatura.textContent = dados[i].temp;
+
+                    var celulaStatus = document.createElement('td');
+                    celulaStatus.textContent = dados[i].Stats;
+
+                    novaLinha.appendChild(celulaHorario);
+                    novaLinha.appendChild(celulaMaquina);
+                    novaLinha.appendChild(celulaTemperatura);
+                    novaLinha.appendChild(celulaStatus);
+
+                    tabela.appendChild(novaLinha);
                 }
             });
         } else {
             throw ('Houve um erro na API!');
         }
-    }).catch(function(resposta){
-        console.error(resposta)
+    }).catch(function (erro) {
+        console.error(erro);
     });
-
 }
+
+carregarRelatorios();
